de523264a814d0a800a6883545cd4b210d121bb2
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2025-06-15 08:04:19 -0500

    fixed improper require import of animate in tailwind config

--- .history/tailwind.config_20241128190343.ts
+++ .history/tailwind.config_20241128190343.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import animate from 'tailwindcss-animate';
 
 export default {
     darkMode: ["class"],
@@ -59,5 +58,5 @@ export default {
   		}
   	}
   },
-  plugins: [animate],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
